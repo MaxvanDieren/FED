42ed8c2109da23e08db56cda1a94423a55eb8709
MaxvanDieren <25459834+MaxvanDieren@users.noreply.github.com>
2022-11-07 13:28:49 +0100

    Hamburger werkend zoals ik wil op p1

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,5 @@
 // JavaScript Document
-console.log("hi");
-var eersteStreepjesButton = document.querySelector("body header nav ul:first-of-type li button");
-eersteStreepjesButton.addEventListener("click", eersteStreepjesButtonKlik);
 
-function eersteStreepjesButtonKlik(){
-    eersteStreepjesButton.classList.toggle("menuOpen");
-}
-
-
-
-//klik op menu spreekt functie aan om te checken of class show menu er is.
-//als dat true is dan verwijdert de class - zo niet voegt het toe
-//verberg ook het open menu icoon en show close
-
-
-//foreach -- blijven klikken op het menu om hem te opnenen of afsluiten 
-//Bij een klik - dus eventlistener
 
 
 
