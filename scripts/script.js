b89f825857bd7181a9798a0ebc1d9a3bc4ff191e
MaxvanDieren <25459834+MaxvanDieren@users.noreply.github.com>
2022-11-08 17:19:21 +0100

    toevoeging micro interactie
    Back to top knop toegevoegd
    img toegevoegd
    hamburger menu werkend zoals ik wil

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
 // JavaScript Document
 
+//BACK TO TOP BUTTON
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 500) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
 
 
 
